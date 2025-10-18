9eb889b67715003460ddbc806ea185d3353f8be3
Ricardo R Barcelar <ricardobarcelar@gmail.com>
2025-10-18 11:48:36 -0300

    PEFT_LoRA Ok; PEFT_PromptTuning em construção;

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -7,4 +7,5 @@ echo "=== Iniciando o Jupyter Notebook ==="
 echo "IP: $IP | Porta: $PORT"
 
 # Inicia o servidor Jupyter
-jupyter notebook --no-browser --ip=$IP --port=$PORT
+#nohup jupyter notebook --no-browser --ip=$IP --port=$PORT
+nohup jupyter notebook --no-browser --ip=$IP --port=$PORT > jupyter.log 2>&1 &
